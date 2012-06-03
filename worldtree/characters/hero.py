1613b9282ef8e0b061f1aa8e0ec071aa2eb41cc9
David Scotton <dscotton@gmail.com>
2012-06-03 00:19:38 -0700

    Made the sprite-changing system more intelligent, and added a POC implementation with a reversed sprite for facing left.

--- worldtree/characters/hero.py
+++ worldtree/characters/hero.py
@@ -20,16 +20,14 @@ STAND = 1
 WALK = 2
 JUMP = 3
 RUN = 4
-LEFT = 1
-RIGHT = 2
+LEFT = controller.LEFT
+RIGHT = controller.RIGHT
+PATH = os.path.join('media', 'sprites')
 
 class Hero(pygame.sprite.Sprite):
   """The player character.
   
   Attributes:
-    images: dict of (state, direction) to list of image Surface objects to show for that
-      state.  If there are multiples, animate by looping through them in order for as
-      long as the state, direction pair is applicable, resetting if it changes.
     image: Surface object that currently represents the character.  Should be changed
       to the appropriate value in the images dict as the character moves.
     environment: the Rect object of the screen the character is moving through.
@@ -37,6 +35,9 @@ class Hero(pygame.sprite.Sprite):
     rect: pygame Rect object representing the sprite's position.
     movement: [x, y] pair representing the character's rate of movement.  (Positive numbers
       are right and down).
+    state: (direction, action) tuple describing what the character is doing.
+    last_state: (direction, action) tuple for the previous frame.
+    animation_frame: int for the frame of animation being shown in a particular action.
       
   Constants:
     SIZE: default (x, y) size of the character. Overridden by the actual size of the current image.
@@ -57,30 +58,43 @@ class Hero(pygame.sprite.Sprite):
   JUMP = 24
   GRAVITY = 2
   TERMINAL_VELOCITY = 8
-  
+    
   def __init__(self, position=(0, 0)):
     pygame.sprite.Sprite.__init__(self)
-    # Load all sprite graphics here.
-    self.images = {
-      (STAND, LEFT) : [os.path.join('media', 'sprites', 'hero_stand_left.png')]
-    }
-    for key in self.images:
-      try:
-        self.images[key] = [pygame.image.load(image) for image in self.images[key]]
-      except pygame.error:
-        placeholder = pygame.Surface(self.SIZE)
-        placeholder.fill(self.COLOR)
-        self.images[key] = [placeholder]
-        
+  
+    # Load the sprite graphics once so we don't need to reload on the fly.
+    # TODO: These ideally would be class constants, but because of the dependency on LoadImage
+    # they can't be without ugly formatting.  See if it's possibly by moving LoadImage to
+    # a higher level.
+    self.STAND_LEFT = Hero.LoadImage('hero_stand_left.png')
+    self.STAND_RIGHT = pygame.transform.flip(self.STAND_LEFT, True, False)
+
     self.environment = pygame.display.get_surface().get_rect()
-    self.direction, self.state = self.DEFAULT_STATE
-    self.image = self.images[(self.direction, self.state)][0]
+    self.action, self.direction = self.DEFAULT_STATE
+    self.image = self.STAND_LEFT
     self.rect = self.image.get_rect()
     self.rect.left, self.rect.top = position
     self.movement = [0, 0]
+    self.last_state = (self.direction, self.action)
+    self.animation_frame = 0
+
+  @property
+  def state(self):
+    return (self.action, self.direction)
+
+  @classmethod
+  def LoadImage(cls, filename):
+    """Load and return a sprite image from its filename."""
+    # TODO: This should really be in a shared class.
+    try:
+      return pygame.image.load(os.path.join(PATH, filename)).convert_alpha()
+    except pygame.error:
+      placeholder = pygame.Surface(cls.SIZE).convert_alpha()
+      placeholder.fill(cls.COLOR)
+      return placeholder
     
   def HandleInput(self):
-    """Handles user input to move the character and change his state."""
+    """Handles user input to move the character and change his action."""
     actions = controller.GetInput()
     if ((controller.LEFT in actions and controller.RIGHT in actions) or
         (controller.LEFT not in actions and controller.RIGHT not in actions)):
@@ -95,44 +109,60 @@ class Hero(pygame.sprite.Sprite):
       self.StopJumping()
   
   def Walk(self, direction):
-    if self.state != JUMP:
-      self.state = WALK
+    if self.action != JUMP:
+      self.action = WALK
     if direction == LEFT:
+      self.direction = LEFT
       self.movement[0] = -self.SPEED
     elif direction == RIGHT:      
+      self.direction = RIGHT
       self.movement[0] = self.SPEED
     
   def StopMoving(self):
-    if self.state != JUMP:
-      self.state = STAND
+    if self.action != JUMP:
+      self.action = STAND
     self.movement[0] = 0
     
   def StopJumping(self):
-    if self.state == JUMP and self.movement[1] < 0:
+    if self.action == JUMP and self.movement[1] < 0:
       self.movement[1] = 0
   
   def Jump(self):
-    if self.state != JUMP:
+    if self.action != JUMP:
       self.movement[1] = self.movement[1] - self.JUMP
-    self.state = JUMP
+    self.action = JUMP
     
   def Supported(self):
-    """The character is standing on something solid. This is the only way to leave JUMP state."""
-    self.state = STAND
+    """The character is standing on something solid. This is the only way to leave JUMP action."""
+    self.action = STAND
     self.movement[1] = 0
     
   def Gravity(self):
     """Decreases the character's upward momentum.
     
-    This should only be called when the character is in the air (in JUMP state).
+    This should only be called when the character is in the air (in JUMP action).
     """
-    assert self.state == JUMP
+    assert self.action == JUMP
     if self.movement[1] < self.TERMINAL_VELOCITY:
       self.movement[1] = min(self.movement[1] + self.GRAVITY, self.TERMINAL_VELOCITY)
 
-  # TODO: handle falling and landing
+  def SetCurrentImage(self):
+    """Sets the image to the appropriate one for the current action, if it exists.
+    
+    This method also handles cycling through frames of animation if there is more than
+    one image for a given state.
+    """
+    # A dict of state->image seemed nice and pythonic at first, but it's not nearly as good
+    # at handling cases where we don't want a different image for every possible state. This
+    # way we can only apply animation logic when there are multiple frames to animate.
+    if LEFT == self.direction:
+      self.image = self.STAND_LEFT
+    elif RIGHT == self.direction:
+      self.image = self.STAND_RIGHT
 
   def update(self):
+    self.SetCurrentImage()
+    self.last_state = self.state
     new_position = self.rect.move(self.movement)
     # TODO: check for obstacles and stuff, besides just the edge of the environment.
     if not self.environment.contains(new_position):
