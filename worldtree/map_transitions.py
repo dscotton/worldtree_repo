7576626e9b3ca9c38ff0ba6affeae6393d3d4b2f
David Scotton <dscotton@gmail.com>
2012-06-17 22:05:40 -0700

    New map data.

--- worldtree/map_transitions.py
+++ worldtree/map_transitions.py
@@ -40,7 +40,8 @@ transitions = {
     LEFT : (Transition(LEFT, 0, 19, 'Map4', 0), 
             Transition(LEFT, 20, 39, 'Map2', -20)),
     DOWN : (Transition(DOWN, 15, 49, 'Map3', -15),),
-    RIGHT : (Transition(RIGHT, 0, 17, 'Map5', 17),)
+    RIGHT : (Transition(RIGHT, 0, 17, 'Map5', 17),
+             Transition(RIGHT, 18, 39, 'Map11', -18),)
   },
   'Map2' : {
     RIGHT : (Transition(RIGHT, 0, 19, 'Map1', 20),),
@@ -48,6 +49,7 @@ transitions = {
   },
   'Map3' : {
     UP : (Transition(UP, 0, 34, 'Map1', 15),),
+    LEFT : (Transition(LEFT, 34, 39, 'Map9', -34),)
   },
   'Map4' : {
     RIGHT : (Transition(RIGHT, 0, 19, 'Map1', 0),),
@@ -56,8 +58,41 @@ transitions = {
   'Map5' : {
     LEFT : (Transition(LEFT, 27, 34, 'Map1', -17),
             Transition(LEFT, 3, 11, 'Map6', -3)),
+    DOWN : (Transition(DOWN, 0, 34, 'Map11', 0),),
   },
   'Map6' : {
     RIGHT : (Transition(RIGHT, 0, 19, 'Map5', 3),),
+  },
+  'Map7' : {
+    RIGHT : (Transition(RIGHT, 0, 5, 'Map8', 27),),
+    LEFT : (Transition(LEFT, 0, 10, 'Map10', 59),),
+  },
+  'Map8' : {
+    RIGHT : (Transition(RIGHT, 0, 5, 'Map9', 74),),
+    LEFT : (Transition(LEFT, 27, 32, 'Map7', -27),),
+  },
+  'Map9' : {
+    RIGHT : (Transition(RIGHT, 0, 5, 'Map3', 34),),
+    LEFT : (Transition(LEFT, 74, 79, 'Map8', -74),),
+  },
+  'Map10' : {
+    RIGHT : (Transition(RIGHT, 0, 9, 'Map12', -60),
+             Transition(RIGHT, 59, 69, 'Map7', 59),),
+  },
+  'Map11' : {
+    LEFT : (Transition(LEFT, 0, 21, 'Map1', 18),),
+    UP : (Transition(UP, 0, 34, 'Map5', 0),),
+    RIGHT : (Transition(RIGHT, 0, 5, 'Map3', 34),),
+  },
+  'Map12' : {
+    LEFT : (Transition(LEFT, 0, 10, 'Map13', -19),
+            Transition(LEFT, 60, 69, 'Map10', 60),),
+    RIGHT : (Transition(RIGHT, 0, 69, 'Map14', 0),),
+  },
+  'Map13' : {
+    RIGHT : (Transition(RIGHT, 19, 29, 'Map12', 19),),
+  },
+  'Map14' : {
+    LEFT : (Transition(LEFT, 0, 69, 'Map12', 0),),
   },
 }
